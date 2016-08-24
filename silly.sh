0f9893604036e19ceb64915f3fb4be5702b21bdb
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-08-24 17:41:10 -0500

    Add Silly Bytes banner

--- silly.sh
+++ silly.sh
@@ -2,9 +2,27 @@
 
 SB_PATH=$HOME/lab/sillybytes
 GH_PAGE=$SB_PATH/sillybytes.github.io
+PROMPT="⚙  "
+
+function banner
+{
+    echo
+    echo
+    echo -e '\e[33m'
+    echo '  ____  _ _ _               ____        _             '
+    echo ' / ___|(_) | |_   _        | __ ) _   _| |_ ___  ___  '
+    echo ' \___ \| | | | | | |       |  _ \| | | | __/ _ \/ __| '
+    echo '  ___) | | | | |_| |       | |_) | |_| | ||  __/\__ \ '
+    echo ' |____/|_|_|_|\__, |       |____/ \__, |\__\___||___/ '
+    echo '              |___/               |___/               '
+    echo -e '\e[0m'
+    echo
+    echo
+}
 
 function deploy
 {
+    banner
     cd $GH_PAGE
     git stash -q --keep-index
     git checkout hakyll
